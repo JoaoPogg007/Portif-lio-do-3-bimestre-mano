a2f91fe93376f5a33d1b62c893f7c526bd569713
JoaoPogg007 <00001111754834sp@al.educacao.sp.gov.br>
2025-09-25 20:27:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botao = document.getElementById('botao-acessibilidade');
+    const opcoes = document.getElementById('opcoes-acessibilidade');
+
+    opcoes.style.display = 'none';
+
+    botao.addEventListener('click', function () {
+        if (opcoes.style.display === 'none') {
+            opcoes.style.display = 'flex';
+        } else {
+            opcoes.style.display = 'none';
+        }
+    });
+
+    let tamanho = 1;
+
+    document.getElementById('aumentar-fonte').addEventListener('click', function () {
+        tamanho += 0.1;
+        document.body.style.fontSize = `${tamanho}rem`;
+    });
+
+    document.getElementById('diminuir-fonte').addEventListener('click', function () {
+        tamanho -= 0.1;
+        document.body.style.fontSize = `${tamanho}rem`;
+    });
+});
+const alternaContraste = document.getElementById('alterna-contraste');
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+
+    // Seleciona todos os cards
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.classList.toggle('alto-contraste-card');
+    });
+})
